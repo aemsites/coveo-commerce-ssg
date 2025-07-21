27dcd974ffeadf7db196fc44c8497498a57c5253
rgravitvl <ravitvl83@gmail.com>
2025-07-21 18:43:50 +0530

    Updated for dot path

--- actions/pdp-renderer/render.js
+++ actions/pdp-renderer/render.js
@@ -220,13 +220,14 @@ async function generateProductHtml(product, ctx, state, dirname = __dirname) {
       product.conjugations = parseJson(product.raw.adconjugationsjson);
       product.notes = parseJson(product.raw?.adnotesjson);
       product.notes?.forEach((note) =>{
-        note.statement = note.statement?.replace(/<a\s([^>]*?href=")(?:\/(?!en-us\/)([^"?"]+)|(\.\.\/)+(\/?[^"?]+))(\?[^"]*)?"/gi,
-          (_, prefix, rootPath, dots, relPath, query = "") => {
+        note.statement = note.statement?.replace(
+          /<a\s([^>]*?href=")(?:\/(?!en-us\/)([^"?"]+)|((?:\.\.\/)+)(\/?[^"?]+))(\?[^"]*)?"/gi,
+          (_, prefix, rootPath, dotSegments, relPath, query = "") => {
             const path = (rootPath || relPath || "").toLowerCase();
             const q = query.toLowerCase();
             return `<a ${prefix}/en-us/${path}${q}"`;
           }
-        )
+        );
       })
       product.images = parseJson(product.raw.imagesjson);
       product.images?.forEach((image) =>{
