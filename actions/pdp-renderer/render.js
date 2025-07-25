3c6ecbd0590dfddef1a123b0bef9795250902ab3
wz914876 <sharanagouda_patil@pall.com>
2025-07-25 20:11:05 +0530

    YETET-3997 Fluorochrome chart links on product notes(detail)

--- actions/pdp-renderer/render.js
+++ actions/pdp-renderer/render.js
@@ -225,6 +225,10 @@ async function generateProductHtml(product, ctx, state, locale, dirname = __dirn
       product.conjugations = parseJson(product.raw.adconjugationsjson);
       product.notes = parseJson(product.raw?.adnotesjson);
       product.notes?.forEach((note) => {
+        note.statement = note.statement?.replace(/href="([^"]*?)"/gi, (match, hrefValue) => {
+          const trimmedHref = hrefValue.trim();
+          return `href="${trimmedHref}"`;
+        });
         note.statement = note.statement?.replace(
             /<a\s+href="https?:\/\/www\.abcam\.com(\/[^"]*)"/gi,
             '<a href="$1"'
